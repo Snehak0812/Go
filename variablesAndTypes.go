a5f3ee6cc19d21ae56d106772753e035e24d8ce8
Snehak0812 <snehasanjaychoudhary04@gmail.com>
2025-07-02 21:30:16 -0500

    Update variablesAndTypes.go

--- variablesAndTypes.go
+++ variablesAndTypes.go
@@ -8,10 +8,14 @@ func main() {
   var name string= "Sneha"
   //const - Values cannot be changed once assigned
   const branch string = "Bengaluru"
-  
+ 
   //Print the values of variables
   fmt.Println("Hi", name,"!")
   fmt.Println("Your account number:",accNo)
   fmt.Println("Your account balance:",accBalance)
   fmt.Println("Your branch:",branch)
+
+  //We can use the := notation to declare and initialize a variable in single line
+  contactNo := 87890
+  fmt.Println("Your contact no:",contactNo)
 }
