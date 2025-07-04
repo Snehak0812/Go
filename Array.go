2bf07f7ef320c4f8be5ba69b60122b6002805b0c
Snehak0812 <snehasanjaychoudhary04@gmail.com>
2025-07-04 00:12:55 -0500

    Update Array.go

--- Array.go
+++ Array.go
@@ -9,9 +9,9 @@ func main() {
     
     //Declaration and Initialization
     nums := [5]int{} //bydefault value is 0
-    fmt.Println("Array:",nums)
+    fmt.Println("Array:",nums, "size:", len(nums))
      
     names := [5]string{"sk","sk","ic"}
-    fmt.Println("Array:",names)
+    fmt.Println("Array:",names, "size:", len(names))
     fmt.Printf("Array: %q\n",names)
-}
+}   
