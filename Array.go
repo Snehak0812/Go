f7ef0360f1670810eb4aceb2b2ab73c6bfcd8ad7
Snehak0812 <snehasanjaychoudhary04@gmail.com>
2025-07-03 23:54:05 -0500

    Create Array.go

---
+++ Array.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+    //Declaration
+    //var nums[5] int
+    //nums[0] = 5
+    //fmt.Println(nums[0])
+    
+    //Declaration and Initialization
+    nums := [5]int{} //bydefault value is 0
+    fmt.Println("Array:",nums)
+     
+    names := [5]string{"sneha","sanjay","ishaan"}
+    fmt.Println("Array:",names)
+}
